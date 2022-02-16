e23501c690065b5ede23bd56c6af8a0720d79a02
Pronixa <pronixa61@gmail.com>
2022-02-16 02:09:26 +0100

    Assignments solved

---
+++ Operators/Operators.js
@@ -0,0 +1,91 @@
+'use strict';
+
+//assignemnt 1
+/*
+let numOfDaysUntilWeekend = 2;
+numOfDaysUntilWeekend -= 1;
+
+1. Use console.log() to log the value just after the variable has been declared
+2.  Use console.log() to log the value right after the value of the variable has been changed. 
+3. Tell what it does 
+*/
+
+let numOfDaysUntilWeekend = 2;
+console.log(numOfDaysUntilWeekend);
+numOfDaysUntilWeekend -= 1;
+console.log(numOfDaysUntilWeekend);
+//We declare variable "numOfDaysUntilWeekend" with value 2 and we decrement its value by 1
+
+//assignemnt 2
+//Addition to numbers
+let assignment2variable1 = 5;
+assignment2variable1 += 2;
+console.log(assignment2variable1);
+
+//assignemnt 3
+//Subtraction to numbers
+let assignment3variable1 = 5;
+assignment3variable1 -= 2;
+console.log(assignment3variable1);
+
+//assignemnt 4
+//write two sentences in two variables and connect them together 
+let assignment4variable1 = "This is variable 1";
+let assignment4variable2 = "this is variable 2";
+let assignment4variable3 = assignment4variable1 + " and " + assignment4variable2;
+console.log(assignment4variable3);
+
+//assignemnt 5
+//write two variables, one number and one string. add them together
+let assignment5variable1 = "Forever ";
+let assignment5variable2 = 21;
+assignment5variable1 += assignment5variable2;
+console.log(assignment5variable1);
+
+//assignemnt 6
+//write the answears in the in a comment
+let r = 15;
+let s = 5;
+let t = r + s; // 20
+let u = r - s; // 10
+let v = t / u; // 2
+let x = r * v; // 30
+let y = s % v; // 1
+
+//assignemnt 7
+//Why does it not write out? 
+let r = 34;
+let c = 28; // changed to 28 from 1
+let x = r - 32 + c;
+let y = r++ * 4; 
+if(x == 30 || y == 2 ){
+ console.log("yeah you got it");
+}
+//change a number in a variable, so it print out the text
+
+
+//assignemnt 8
+//make two vaiables. One variable must contain a number and the other one must contain a prompt, 
+//where the user can enter a number. Match the two number and print out the answear
+//TIP: search prompt
+
+let assignment8variable1 = 5;
+let assignment8variable2 = prompt("Please enter a whole number:", "5");
+console.log(assignment8variable2);
+
+//assignment 9
+//write these numbers in the console and write the output. Explain you thing this happens
+    0.1 + 0.2;
+    0.1 + 0.2 === 0.3;
+    3  - 1
+    3  + 1
+    '3' - 1 
+    '3' + 1 
+    '222' - -'111'
+
+
+//assignemnt 9
+//Write a JavaScript program to determine whether a given year is a leap year 
+
+
+
